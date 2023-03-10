8150107c05edc414eed4933d73ba42a35eb63c12
Tima <timagiyazov@gmail.com>
2023-03-10 14:52:05 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Yelpcamp
+
+A fullstack project from the Udemy course "The Web Developer Bootcamp 2022" by Colt Steele. 
+
+# Screenshot
+![image](https://user-images.githubusercontent.com/82305211/209966418-55132134-9d57-4a51-86e3-4ca80a992f7f.png)
+![image](https://user-images.githubusercontent.com/82305211/209966469-bc92999c-43fd-4b8e-8efb-5ea834aa6131.png)
+![image](https://user-images.githubusercontent.com/82305211/209966594-bf7ea005-ef08-4b02-9e81-426f270c518e.png)
+
+
+
+
+# Demo
+<a href="https://yelpcamp-d1tm.onrender.com/" target="_blank">Click me</a> to view the demo
+
+<ul>
+<li>Username: testuser</li>
+<li>Password: testuser</li>
+
+</ul>
+
+
+# Built with
+
+<b>Frontend</b>
+<ol>
+<li>Bootstrap 5</li>
+<li>Ejs</li>
+</ol>
+
+<b>Backend</b>
+<ol>
+<li>Express</li>
+<li>Mongoose</li>
+<li>Method-override</li>
+<li>Joi</li>
+<li>Passport.js</li>
+</ol>
---
+++ seeds/index.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const {places, descriptors} = require("./seedHelpers");
+const Campground = require("../models/campground");
+
+const photoArray = [
+  "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+  "https://images.unsplash.com/photo-1532339142463-fd0a8979791a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  "https://images.unsplash.com/photo-1478131143081-80f7f84ca84d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  "https://images.unsplash.com/photo-1517824806704-9040b037703b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  "https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  "https://images.unsplash.com/photo-1537225228614-56cc3556d7ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  "https://images.unsplash.com/photo-1496545672447-f699b503d270?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+  "https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+  "https://images.unsplash.com/photo-1601134917279-ef70a0a90f18?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+  "https://images.unsplash.com/photo-1563299796-17596ed6b017?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+  "https://images.unsplash.com/photo-1510672981848-a1c4f1cb5ccf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1744&q=80",
+  "https://images.unsplash.com/photo-1529385101576-4e03aae38ffc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+  "https://images.unsplash.com/photo-1602391833977-358a52198938?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80",
+  "https://images.unsplash.com/photo-1475483768296-6163e08872a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+  "https://images.unsplash.com/photo-1508873696983-2dfd5898f08b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+];
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 200; i++) {
+    const random15 = Math.floor(Math.random() * 15); //photo random
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
+    const camp = new Campground({
+      author:  '6403416cacb83a0654b64939',
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quos pariatur magni, iure itaque vitae maxime tempora provident veritatis beatae expedita suscipit adipisci temporibus saepe laudantium ipsam accusamus quasi ratione!",
+      price: price,
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
+      images: [
+        {
+          // url: "https://res.cloudinary.com/dl0xf6jnb/image/upload/v1671278229/YelpCamp/zbzlfjdmqf3echo86bbz.png",
+          url: photoArray[random15],
+          filename: "YelpCamp/it2ronublo6hif6m88yk",
+        },
+      ],
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
